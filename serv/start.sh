dc51f35531056939ea832fd661c52899821a16aa
ludovicplt <ludovic.peltier@epitech.eu>
2022-09-17 13:45:35 +0400

    push

--- serv/start.sh
+++
@@ -1,1 +0,0 @@
-java -Xmx2048M -jar spigot-1.12.2.jar
--- untitled/src/main/java/com/ludovicplt/plugin/plugin.java
+++ untitled/src/main/java/com/ludovicplt/plugin/plugin.java
@@ -7,7 +7,9 @@ public class plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         getServer().getPluginManager().registerEvents(new Events(), this);
+
     }
     @Override
     public void onDisable() {
